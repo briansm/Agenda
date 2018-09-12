a9a669f97b9a06e3257fe29db4c2cfb231bfc15a
Brian Morales <briansmor09@gmail.com>
2018-09-11 20:43:37 -0600

    Primeras Pruebas del metodo agregar

--- Contactos.py
+++ Contactos.py
@@ -1,11 +1,28 @@
 # -*- Coding: utf-8 -*-
 
+class Contacts:
+#-----------Constructor-----------------
+    def __init__(self, nombre, correo, telefono):
+#-------------_nombre es equivalente a variable privada
+        self._nombre = nombre
+        self._correo = correo
+        self._telefono = telefono
 
+class ContactBook:
+
+    def __init__(self, ):
+        self._contacts=[]
+
+    def add(self,nombre,correo,telefono):
+        print("nombre: {} correo: {} telefono: {}".format(nombre,correo,telefono))
 
 
 
 
 def run():
+#--------instancia del objeto COntactBook
+    contact_book= ContactBook()
+
     while True:
         opcion=int(input('''
                 ---------BIENVENIDO-----------
@@ -21,16 +38,24 @@ def run():
 
                 '''))
 
-                if opcion==1:
-                    print("Agregando...")
-                elif opcion==2:
-                    print("Actualizando...")
-                elif opcion==3:
-                    print("Buscando...")
-                elif opcion==4:
-                    print("Eliminando...")
-                elif opcion==5:
-                    print("Mostrando...")
-                elif opcion==6:
-                    print("Saliendo")
-                    break
+        if opcion==1:
+            nombre=str(raw_input("Nombre Contacto: "))
+            correo=str(raw_input("Correo Electronico: "))
+            telefono=int(input("Numero de Telefono: "))
+            contact_book.add(nombre,correo, telefono)
+            print("Agregando...")
+            
+        elif opcion==2:
+            print("Actualizando...")
+        elif opcion==3:
+            print("Buscando...")
+        elif opcion==4:
+            print("Eliminando...")
+        elif opcion==5:
+            print("Mostrando...")
+        elif opcion==6:
+            print("Saliendo")
+            break
+
+if __name__=='__main__':
+    run()
